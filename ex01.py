e8527a7ebc274c72fbf6dd533801d84c5554f3b3
C0A22083 <c0a22083e0@edu.teu.ac.jp>
2023-09-26 15:32:56 +0900

    こうかとん反転

--- ex01.py
+++ ex01.py
@@ -7,15 +7,19 @@ def main():
     screen = pg.display.set_mode((1600, 900))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    img = pg.image.load("ex01/fig/3.png")
+    img2 = pg.transform.flip(img, True, False)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
+        screen.blit(img, [tmr,300])
+        screen.blit(img2, [tmr,400])
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(60)
     
     
 if __name__ == "__main__":
