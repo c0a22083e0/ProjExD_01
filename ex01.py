8c3dba0e4304eec16f8b91aa7b264be7f526febe
C0A22083 <c0a22083e0@edu.teu.ac.jp>
2023-09-26 18:21:29 +0900

    滑らかに動くこうかとん

--- ex01.py
+++ ex01.py
@@ -12,22 +12,33 @@ def main():
     tmr = 0
     t = 0
     k = 1
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         img3 = pg.transform.rotate(img2, t)
         img_list = [img2,img3]
         screen.blit(bg_img, [tmr, 0])
-        screen.blit(bg_img, [1600+tmr,0])
+        screen.blit(pg.transform.flip(bg_img, True, False), [1600 + tmr,0])
+        screen.blit(bg_img, [3200 + tmr,0])
+        screen.blit(pg.transform.flip(bg_img, True, False), [4800 + tmr,0])
+    
         screen.blit(img_list[1], [300 ,200])
-        pg.display.update()
-        tmr -= 1      
+
+        tmr -= 1   
+        if tmr < -3200:
+    
+            tmr = 0
+              
         t += k  
         if t > 10:
             k *= -1
         elif t < 0:
             k *= -1
-        clock.tick(30)
+
+        clock.tick(300)
+        pg.display.update()
+        
     
     
 if __name__ == "__main__":
